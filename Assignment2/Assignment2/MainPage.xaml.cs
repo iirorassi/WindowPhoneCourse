684ccf017b2c6e32f8c86c770484df52d455b702
iirorassi <iiro.rassi@gmail.com>
2015-03-21 22:12:48 +0200

    Assignment2
    Assignment 2: Fetching weather information as Json, and handling
    lifecycles for that data

---
+++ Assignment2/Assignment2/MainPage.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace Assignment2
+{
+
+    public sealed partial class MainPage : Page
+    {
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+           
+        }
+
+        //Functionality for searching for weather information
+        private async void Button_Search(object sender, RoutedEventArgs e)
+        {
+            //Send a request for weather info from openweathermap.org
+            try {
+                using (HttpClient hc = new HttpClient()){
+                    hc.BaseAddress = new Uri("http://api.openweathermap.org");
+                    //This defines what is queried "weather", mode "json, units "metric" and developer specific "API key"
+                    var url = "data/2.5/weather?q={0}&mode=json&units=metric&APPID=b9dc06ad1c836d309308e41729e41a39";
+                    hc.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    HttpResponseMessage response = await hc.GetAsync(String.Format(url, InputBox.Text));
+                    
+                    //Write debug information
+                    System.Diagnostics.Debug.WriteLine("Response: " + response.StatusCode + " ---- " + response.ToString());
+
+                    //If HttpResponseMessage returns success code write received data to string and send to WeatherInfo page
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var data = response.Content.ReadAsStringAsync();
+                        String SendContent = data.Result.ToString();
+
+                        //Here it would be useful to parse the Json data
+
+                        //Navigate to WeatherInfo page and pass the weatherinformation as a parameter
+                        this.Frame.Navigate(typeof(WeatherInfo),SendContent);
+                    }
+                }
+            } 
+                //If not successful, give feedback to user
+            catch (Exception ex){
+                System.Diagnostics.Debug.WriteLine("Exception: " + ex.ToString());
+                InputBox.Text = "Error occured, sorry.";
+            }
+            
+        }
+
+        // Functionality for navigating to WeatherInfo page
+        private void Button_Weather(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(WeatherInfo));
+        }
+    }
+}
+
---
+++ Assignment2/Assignment2/WeatherInfo.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+
+namespace Assignment2
+{
+    public sealed partial class WeatherInfo : Page
+    {
+        
+        public WeatherInfo()
+        {
+            this.InitializeComponent();
+
+        }
+
+
+        //When navigating to this page, parameter is received and put into ContentBox
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+
+            var parameter = e.Parameter as String;
+            //When navigating to WeatherInfo page without doing a new search, old data must be retrieved
+                if (parameter == null)
+                {
+                    //Null parameter = check for saved data in LocalSetting
+                    Windows.Storage.ApplicationDataContainer localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+                    if (localSettings.Values.ContainsKey("Val"))
+                    {
+                        ContentBox.Text = (localSettings.Values["Val"] as string).ToString();
+                        //Debug info
+                        System.Diagnostics.Debug.WriteLine("*_*_*_This data loaded from LocalSettings:");
+                        System.Diagnostics.Debug.WriteLine((localSettings.Values["Val"] as string).ToString());
+                    }
+
+                }
+                else { 
+
+                    //If not null, there is weatherdata in the parameter
+                    //Display this data in ContentBox
+                ContentBox.Text = parameter;
+                    //Save ContentBox Text to LocalSettings
+                Windows.Storage.ApplicationDataContainer localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+                localSettings.Values["Val"] = ContentBox.Text;
+                    //Debug info
+                System.Diagnostics.Debug.WriteLine("*_*_*_This data saved to LocalSettings:");
+                System.Diagnostics.Debug.WriteLine((localSettings.Values["Val"] as string).ToString());
+                }
+
+        }
+
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+  
+        }
+
+
+
+        //Functionality for navigation back to MainPage
+        //This is not good WP8.1 design, next step implementing bottom menubar or physical back button controls.
+        private void Button_Back(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage));
+        }
+    }
+}
